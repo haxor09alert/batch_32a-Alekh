e24d7ce9117622755450166f0da519be1268be85
Alekh Chaudhary <chaudharyalekh@gmail.com>
2024-04-20 00:17:04 +1000

    project files setup-1

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,6 @@
+
+
+const kAppBar = (0xffF2F3D8);
+const kBackground = (0XFFFFFFFF); //WHITE
+// const kBackground = (0X000000);//BLACK
+const kText800 =(0xFF410D59);
---
+++ lib/core/constants/string_constants.dart
@@ -0,0 +1,6 @@
+const kSkip = "Skip";
+const KWardrobe = "WARDROBE";
+const kPopularNow = "Popular Now";
+const kHello = "Hello";
+const kForgetPassword = "'Follow a few simple steps to reset it and regain access to your account effortlessly.'";
+const kOtp = "'We have sent you an OTP code in your Email; please enter the code.'";
